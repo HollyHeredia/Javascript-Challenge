8bea7409c81872a07d8d3eaeec01ffd20af7b83a
Holly Heredia <hollyheredia@gmail.com>
2021-07-21 19:45:59 -0700

    Added data to table.

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// add data to table
+var tbody = d3.select("tbody");
+
+
+data.forEach(tableData => {
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
